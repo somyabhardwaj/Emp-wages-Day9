65ccd645e4ba6ef085eb26541b8dec89883f56e2
somyabhardwaj <somyabhardwaj004@gmail.com>
2022-08-03 23:44:42 +0530

    Wages

--- empwages.sh
+++ empwages.sh
@@ -4,7 +4,7 @@ echo "Welcome to employee wage management system"
 rand=$(($RANDOM%2))
 if [ $rand -eq 0 ]
 then
-	echo "Employee is absent"
+        echo "Employee is absent"
 else
-	echo "Employee is present"
+        echo "Employee is present"
 fi
